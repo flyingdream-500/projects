b3bdd545862be283a2e3f549a09ab5932c5c7a6b
flyingdream-500 <57061500+flyingdream-500@users.noreply.github.com>
2021-10-26 21:55:18 +0300

    Realization fun getVerifyCodeFromMessage() by Regex

--- app/src/main/java/com/example/smsreciever/Utils.kt
+++ app/src/main/java/com/example/smsreciever/Utils.kt
@@ -4,9 +4,17 @@ const val SMS_RECEIVE_CODE = 98
 const val CALLER_NUMBER = "900"
 private const val START_CHAR = ": "
 private const val END_CHAR = "."
+private const val NUMBER_REGEX = "[0-9]+"
 
-//get verification code from sms body
+//get verification code from sms body by Regex
 fun getVerifyCodeFromMessage(message: String): String {
+    val regex = NUMBER_REGEX.toRegex()
+    val matcher = regex.find(message)
+    return matcher?.value ?: ""
+}
+
+//get verification code from sms body by substring
+fun oldVerifyCodeFromMessage(message: String): String {
     return message
             .substringAfter(START_CHAR)
             .substringBefore(END_CHAR)
